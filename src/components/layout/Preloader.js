9c8a2fc5d72350a6ea11ff7f2846165c796b92d5
stephin007 <stephinreji123@gmail.com>
2020-12-07 07:22:05 +0530

    Added the Preloader Component

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <div className="progress blue lighten-4">
+            <div class="indeterminate blue" />
+        </div>
+    )
+}
+
+export default Preloader
