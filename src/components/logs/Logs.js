396858ac252b7fe80817a5ea22aa37d8f3e7c0ea
stephin007 <stephinreji123@gmail.com>
2020-12-10 07:39:01 +0530

    Used redux to get the fucntions from actions

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux"
+import PropTypes from 'prop-types'
+
 import LogItem from "./LogItem"
+import {getLogs} from "../../actions/logActions"
 import Preloader from "../layout/Preloader"
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
+const Logs = ({log:{logs, loading}, getLogs}) => {
 
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch("/logs");
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs===null) {
     return <Preloader/>;
   }
 
@@ -38,4 +31,13 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  log: state.log
+})
+
+export default connect(mapStateToProps,{getLogs})(Logs);
